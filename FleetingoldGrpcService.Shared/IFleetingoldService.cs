f9b0f691d7a9930f821cceeefabf724b398c93cb
fleetingold <1161851523@qq.com>
2021-06-25 17:02:47 +0800

    refact folder

---
+++ FleetingoldGrpcService.Shared/IFleetingoldService.cs
@@ -0,0 +1,13 @@
+﻿using MagicOnion;
+using System;
+
+namespace FleetingoldGrpcService.Shared
+{
+    // Defines .NET interface as a Server/Client IDL.
+    // The interface is shared between server and client.
+    public interface IFleetingoldService : IService<IFleetingoldService>
+    {
+        // The return type must be `UnaryResult<T>`.
+        UnaryResult<int> SumAsync(int x, int y);
+    }
+}
