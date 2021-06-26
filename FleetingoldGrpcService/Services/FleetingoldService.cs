ad7281c2baae342eaf40d1ba7e908d2d6432a441
fleetingold <1161851523@qq.com>
2021-06-26 12:26:46 +0800

    Dependency Injection: Microsoft.Extensions.Logging.ILogger<IFleetingoldService>

--- FleetingoldGrpcService/Services/FleetingoldService.cs
+++ FleetingoldGrpcService/Services/FleetingoldService.cs
@@ -1,6 +1,7 @@
 ﻿using FleetingoldGrpcService.Shared;
 using MagicOnion;
 using MagicOnion.Server;
+using Microsoft.Extensions.Logging;
 using System;
 
 namespace FleetingoldGrpcService.Services
@@ -9,10 +10,20 @@ namespace FleetingoldGrpcService.Services
     // The implementation class must inehrit `ServiceBase<IFleetingoldService>` and `IFleetingoldService`
     public class FleetingoldService : ServiceBase<IFleetingoldService>, IFleetingoldService
     {
+        private readonly ILogger logger;
+
+        public FleetingoldService(ILogger<IFleetingoldService> logger)
+        {
+            this.logger = logger;
+        }
+
         // `UnaryResult<T>` allows the method to be treated as `async` method.
         public async UnaryResult<int> SumAsync(int x, int y)
         {
-            Console.WriteLine($"Received:{x}, {y}");
+            logger.LogInformation($"---------------------------------- param x:{x},y:{y}");
+            //Console.WriteLine($"Received:{x}, {y}");
+            logger.LogInformation($"---------------------------------- result: {x + y}");
+
             return x + y;
         }
     }
